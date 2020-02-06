d3a6e2c5fced0f9ac493ff1e5ef399d3c8cdf074
Marina Mezentseva <marinamezentseva07@gmail.com>
2020-02-07 00:34:07 +0300

    task_3_more_details_er_correction

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 'use strict';
 
-var AVATARS = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
 var TITLES = ['Комфортабельная квартира возле городского парка', 'Вы нашли то, что искали. Приезжайте прямо сейчас'];
-var ADDRESSES = ['600, 350', '450, 300', '570, 520', '130, 400'];
 var PRICES = [3400, 4700, 5200, 7800, 11600];
 var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var ROOMS = [1, 2, 3, 4, 5];
@@ -12,47 +10,46 @@ var CHECKOUTS = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var DESCRIPTIONS = ['Великолепная квартира-студия в центре Токио. Подходит как туристам, так и бизнесменам. Квартира полностью укомплектована и недавно отремонтирована.', 'Роскошная квартира в живописном, престижном районе'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var adsNumber = 8;
+var ADS_NUMBER = 8;
 var LOCATION_Y_MIN = 130;
 var LOCATION_Y_MAX = 630;
+var MIN_VALUE = 0;
 
-document.querySelector('.map').classList.remove('map--faded');
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
 
-var getRandomNumber = function (length) {
-  return Math.floor(Math.random() * length);
+var getRandomNumberInRange = function (max, min) {
+  min = min || MIN_VALUE;
+  return Math.floor(Math.random() * (max - min)) + min;
 };
 
 var getRandomElementFromArray = function (array) {
-  return array[getRandomNumber(array.length)];
-};
-
-var getRandomAvatar = function () {
-  var rand = getRandomNumber(AVATARS.length);
-  var uniqueItem = AVATARS[rand];
-  AVATARS.splice(rand, 1);
-  return uniqueItem;
+  return array[getRandomNumberInRange(array.length)];
 };
 
 var getRandomLengthArray = function (array) {
   var randomLengthArray = [];
-  for (var i = 0; i <= getRandomNumber(array.length + 1); i++) {
+  var randomLength = getRandomNumberInRange(array.length);
+  for (var i = 0; i <= randomLength; i++) {
     randomLengthArray.push(array[i]);
   }
   return randomLengthArray;
 };
 
-var getRandomLocation = function (min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+var getAvatar = function (id) {
+  return 'img/avatars/user0' + id + '.png';
 };
 
-var createRandomAd = function () {
+var createRandomAds = function (avatarID) {
+  var xCoordinate = getRandomNumberInRange(map.clientWidth);
+  var yCoordinate = getRandomNumberInRange(LOCATION_Y_MAX, LOCATION_Y_MIN);
   return {
     author: {
-      avatar: getRandomAvatar()
+      avatar: getAvatar(avatarID)
     },
     offer: {
       title: getRandomElementFromArray(TITLES),
-      address: getRandomElementFromArray(ADDRESSES),
+      address: xCoordinate + ',' + yCoordinate,
       price: getRandomElementFromArray(PRICES),
       type: getRandomElementFromArray(TYPES),
       rooms: getRandomElementFromArray(ROOMS),
@@ -64,32 +61,35 @@ var createRandomAd = function () {
       photos: getRandomLengthArray(PHOTOS)
     },
     location: {
-      x: getRandomLocation(LOCATION_Y_MIN, LOCATION_Y_MAX),
-      y: getRandomLocation(LOCATION_Y_MIN, LOCATION_Y_MAX)
+      x: xCoordinate,
+      y: yCoordinate
     }
   };
 };
 
 var generateAd = function (number) {
   var ads = [];
-  for (var i = 0; i < number; i++) {
-    ads.push(createRandomAd());
+  for (var i = 1; i <= number; i++) {
+    ads.push(createRandomAds(i));
   }
   return ads;
 };
 
-var advertisements = generateAd(adsNumber);
+var advertisements = generateAd(ADS_NUMBER);
 
 var similarListElement = document.querySelector('.map__pins');
 var similarElementTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
+var xOffset = similarElementTemplate.offsetWidth;
+var yOffset = similarElementTemplate.offsetTop;
+
 var renderPinTemplate = function (pin) {
   var newPinElement = similarElementTemplate.cloneNode(true);
 
-  newPinElement.style.left = pin.location.x;
-  newPinElement.style.top = pin.location.y;
-  newPinElement.src = pin.avatar;
-  newPinElement.alt = pin.title;
+  newPinElement.style.left = pin.location.x - xOffset + 'px';
+  newPinElement.style.top = pin.location.y - yOffset + 'px';
+  newPinElement.querySelector('img').src = pin.author.avatar;
+  newPinElement.querySelector('img').alt = pin.offer.title;
 
   return newPinElement;
 };
